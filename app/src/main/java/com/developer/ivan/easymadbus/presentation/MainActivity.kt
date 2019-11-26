1784177994b0dd4bb941fbffffe04fc6a0e11949
Exensor <ivanfr89@gmail.com>
2019-11-26 19:20:46 +0100

    Add three basic views with NavController to the MainActivity

--- app/src/main/java/com/developer/ivan/easymadbus/presentation/MainActivity.kt
+++ app/src/main/java/com/developer/ivan/easymadbus/presentation/MainActivity.kt
@@ -2,12 +2,36 @@ package com.developer.ivan.easymadbus.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.developer.ivan.easymadbus.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+
+//        val host = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.mapFragment,R.id.favouriteFragment,R.id.notificationFragment))
+
+        setupActionBarWithNavController(navController,appBarConfiguration)
+        setupBottomNavigationItems(navController)
     }
+
+    private fun setupBottomNavigationItems(navController: NavController)
+    {
+        nav_view.setupWithNavController(navController)
+    }
+
+
 }
