87c2656ba76538383f965265522f4e49c9bdb12f
Exensor <ivanfr89@gmail.com>
2019-12-22 22:09:59 +0100

    WIP: Bus stops

--- app/src/main/java/com/developer/ivan/easymadbus/core/Failure.kt
+++ app/src/main/java/com/developer/ivan/easymadbus/core/Failure.kt
@@ -1,11 +1,14 @@
 package com.developer.ivan.easymadbus.core
 
+import java.lang.Exception
+
 sealed class Failure
 {
     object ConnectivityError: Failure()
-    object JsonException: Failure()
+    class JsonException(val body: String): Failure()
 
-    class ServerError(errorCode: Int): Failure()
+    class ServerError(val errorCode: String): Failure()
+    class ServerException(val serverException: Throwable): Failure()
 
     abstract class FailureAbstract: Failure()
 }
