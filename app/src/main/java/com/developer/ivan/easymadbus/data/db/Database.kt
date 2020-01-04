7a2dd8efd25e55d37ef1d56f359fb55855d7c809
Exensor <ivanfr89@gmail.com>
2020-01-04 17:07:29 +0100

    Close #24
    Token stored for offline version

--- app/src/main/java/com/developer/ivan/easymadbus/data/db/Database.kt
+++ app/src/main/java/com/developer/ivan/easymadbus/data/db/Database.kt
@@ -3,12 +3,15 @@ package com.developer.ivan.easymadbus.data.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.developer.ivan.easymadbus.data.db.dao.BusStopDao
+import com.developer.ivan.easymadbus.data.db.dao.TokenDao
 import com.developer.ivan.easymadbus.data.db.models.DBBusStop
+import com.developer.ivan.easymadbus.data.db.models.DBToken
 
-@Database(entities = [DBBusStop::class], version = 1)
+@Database(entities = [DBBusStop::class, DBToken::class], version = 2)
 abstract class Database : RoomDatabase() {
 
     abstract fun busStopDao(): BusStopDao
+    abstract fun tokenDao(): TokenDao
 //    abstract fun busStopFavourite(): BusStopFavouriteDao
 
 }
--- app/src/main/java/com/developer/ivan/easymadbus/domain/models/DomainModels.kt
+++ app/src/main/java/com/developer/ivan/easymadbus/domain/models/DomainModels.kt
@@ -1,25 +1,31 @@
 package com.developer.ivan.easymadbus.domain.models
 
 import android.os.Parcelable
+import com.developer.ivan.easymadbus.core.Constants
 import com.developer.ivan.easymadbus.data.db.models.DBBusStop
 import com.developer.ivan.easymadbus.data.db.models.DBGeometry
-import com.developer.ivan.easymadbus.data.server.models.EntityGeometry
+import com.developer.ivan.easymadbus.data.db.models.DBToken
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.clustering.ClusterItem
 import kotlinx.android.parcel.Parcelize
+import java.util.concurrent.TimeUnit
 
 @Parcelize
-data class Token(val accessToken: String, val tokenSecExpiration: Int) : Parcelable
+data class Token(val accessToken: String, val tokenSecExpiration: Int, val timeStamp: Long) : Parcelable
+{
+    fun isExpired() =
+        TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - timeStamp) >= tokenSecExpiration
 
+    fun toDb(): DBToken = DBToken(Constants.Token.TOKEN_ID,accessToken,tokenSecExpiration,timeStamp)
+
+}
 @Parcelize
 data class BusStop(
     val node: String,
     val geometry: Geometry,
     val name: String,
     val wifi: String,
-    val lines: List<String>,
-    val favourite: Boolean
-) : Parcelable, ClusterItem {
+    val lines: List<String>) : Parcelable, ClusterItem {
     override fun getSnippet(): String = name
 
     override fun getTitle(): String = name
