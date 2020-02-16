d8abbeeb3dace1c04fb3218b196e19f306bf9f3f
Iván <6018467+ivfr89@users.noreply.github.com>
2020-02-16 16:22:26 +0100

    Revert "Fix pass"
    This reverts commit c71fe20c766db48b54830a6277cec610c221dd93.

--- app/src/main/java/com/developer/ivan/easymadbus/core/Constants.kt
+++ app/src/main/java/com/developer/ivan/easymadbus/core/Constants.kt
@@ -12,7 +12,7 @@ object Constants {
 
     object EMTApi {
         const val USER_EMAIL = "ivanfr89@gmail.com"
-        const val USER_PASSWORD = "Mm7777"
+        const val USER_PASSWORD = "12345"
         const val API_KEY = "0641b5f4-2441-47d3-981e-05d429f61ac7"
         const val CLIENT_KEY = "fc9da71e-87e5-4137-9140-7ae59547310f"
         const val ENDPOINT = "https://openapi.emtmadrid.es/v1/"
