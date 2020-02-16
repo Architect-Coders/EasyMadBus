bd98d77c3f9afb8da18b2a5eb669dd9b0e93c401
Iván <6018467+ivfr89@users.noreply.github.com>
2020-02-16 16:22:26 +0100

    Revert "Added Detekt"
    This reverts commit a4a664a03f5de111b87043eb0b97badcb7dc43bb.

--- app/src/main/java/com/developer/ivan/easymadbus/core/CoreExtensions.kt
+++ app/src/main/java/com/developer/ivan/easymadbus/core/CoreExtensions.kt
@@ -6,5 +6,4 @@ import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 
 
-fun ViewGroup.inflateFragment
-            (@LayoutRes layout: Int): View = LayoutInflater.from(this.context).inflate(layout,this,false)
+fun ViewGroup.inflateFragment(@LayoutRes layout: Int): View = LayoutInflater.from(this.context).inflate(layout,this,false)
