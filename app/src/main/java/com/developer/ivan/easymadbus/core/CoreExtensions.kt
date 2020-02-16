f33f6083b760e73912b521bbbd979739653d75e2
Iván <6018467+ivfr89@users.noreply.github.com>
2020-02-16 16:22:26 +0100

    Revert "Added return type for warning in inflateFragment"
    This reverts commit d47f533f8162f56ec2bbb9cbf54379ed578ac956.

--- app/src/main/java/com/developer/ivan/easymadbus/core/CoreExtensions.kt
+++ app/src/main/java/com/developer/ivan/easymadbus/core/CoreExtensions.kt
@@ -1,9 +1,8 @@
 package com.developer.ivan.easymadbus.core
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 
 
-fun ViewGroup.inflateFragment(@LayoutRes layout: Int): View = LayoutInflater.from(this.context).inflate(layout,this,false)
+fun ViewGroup.inflateFragment(@LayoutRes layout: Int) = LayoutInflater.from(this.context).inflate(layout,this,false)
