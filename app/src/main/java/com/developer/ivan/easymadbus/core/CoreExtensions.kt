75f0f42e096519b5bc580f5f4b2cd9bc90dcb993
Iván <6018467+ivfr89@users.noreply.github.com>
2020-02-16 16:22:26 +0100

    Revert "DRY in fragments. Using extension function inflateFragment"
    This reverts commit ee7f438c02f51584f025465a6c54c24685e0ef65.

--- app/src/main/java/com/developer/ivan/easymadbus/core/CoreExtensions.kt
+++
@@ -1,8 +0,0 @@
-package com.developer.ivan.easymadbus.core
-
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import androidx.annotation.LayoutRes
-
-
-fun ViewGroup.inflateFragment(@LayoutRes layout: Int) = LayoutInflater.from(this.context).inflate(layout,this,false)
--- app/src/main/java/com/developer/ivan/easymadbus/presentation/notifications/NotificationFragment.kt
+++ app/src/main/java/com/developer/ivan/easymadbus/presentation/notifications/NotificationFragment.kt
@@ -8,7 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.developer.ivan.easymadbus.R
-import com.developer.ivan.easymadbus.core.inflateFragment
 
 /**
  * A simple [Fragment] subclass.
@@ -18,7 +17,10 @@ class NotificationFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? =  container?.inflateFragment(R.layout.fragment_notification)
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_notification, container, false)
+    }
 
 
 }
